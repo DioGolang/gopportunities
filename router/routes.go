bd695742f223df42623577a8ee7ec335e0237aab
DVasconcelos <di0g0vasconcelos@hotmail.com>
2024-07-09 16:46:23 -0300

    feat: add routes logic

---
+++ router/routes.go
@@ -0,0 +1,43 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func initializeRoutes(r *gin.Engine) {
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening",
+			})
+		})
+
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST Opening",
+			})
+		})
+
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening",
+			})
+		})
+
+		v1.PUT("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening",
+			})
+		})
+
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening",
+			})
+		})
+
+	}
+
+}
